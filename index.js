e29dd64f0500655d1a217f50315ad0be60d7b4e6
Eric C <erix2205@gmail.com>
2020-07-14 14:20:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object,key){
-  var newObj = Object.assign({},object)
+  newObj = Object.assign({},object)
   delete newObj.key
   return newObj
 }
