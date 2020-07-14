4295f31c4326cf41847a64ade4f7974c7f6f6f79
Eric C <erix2205@gmail.com>
2020-07-14 14:14:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function destructivelyDeleteFromObjectByKey(object,key){
-  delete object.key
-  return object
-}
-
 function deleteFromObjectByKey(object,key){
   var newObj = object
   delete newObj.key
   return newObj
+}
+
+
+function destructivelyDeleteFromObjectByKey(object,key){
+  delete object.key
+  return object
 }
